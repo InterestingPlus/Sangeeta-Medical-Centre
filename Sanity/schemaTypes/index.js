961d3edbf7615eab688abf454673f144682c09a5
Jatin Poriya <kiritporiya25@gmail.com>
2025-07-30 22:46:57 +0530

    Added Sanity Codebase

---
+++ Sanity/schemaTypes/index.js
@@ -0,0 +1,25 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import comment from './comment'
+import blog from './blog'
+import specialty from './specialty'
+import review from './review'
+import doctor from './doctor'
+import infra from './infra'
+
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  comment,
+  blockContent,
+
+  blog,
+  specialty,
+  review,
+  doctor,
+  infra,
+  //   procedure,
+]
